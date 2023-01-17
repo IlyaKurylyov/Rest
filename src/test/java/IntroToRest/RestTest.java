208625d26fe924852dc128403815be339053cf9c
Quiz <ilya.kurylyov94@gmail.com>
2023-01-17 14:35:14 +0300

    IntroToRest

--- src/test/java/IntroToRest/RestTest.java
+++ src/test/java/IntroToRest/RestTest.java
@@ -1,6 +1,8 @@
 package IntroToRest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -9,12 +11,16 @@ import static org.hamcrest.Matchers.*;
 
 public class RestTest {
 
-    @Test
-    public void GetOrder(){
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10})
+    public void parameter(int id){
+
         given()
                 .accept("*/*")
         .when()
-                .get("http://51.250.6.164:8080/test-orders/{id}",1)
+                .get("http://51.250.6.164:8080/test-orders/{id}",id)
                 .then()
                 .statusCode(200)
                 .body("status",equalTo("OPEN"),
