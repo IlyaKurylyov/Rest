d59b81a3c7999c0994a61c8d79131a7a87b5346e
Quiz <ilya.kurylyov94@gmail.com>
2023-01-18 19:42:27 +0300

    IntroToRest

---
+++ src/main/java/rest/dto/Order.java
@@ -0,0 +1,58 @@
+package rest.dto;
+
+public class Order {
+    private String status;
+    private Integer courierId;
+    private String customerName;
+    private String customerPhone;
+    private String comment;
+    private Integer id;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getCourierId() {
+        return courierId;
+    }
+
+    public void setCourierId(Integer courierId) {
+        this.courierId = courierId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/org/example/RestTest.java
@@ -0,0 +1,34 @@
+package org.example;
+import com.google.gson.Gson;
+import rest.dto.Order;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.post;
+import static org.hamcrest.Matchers.*;
+
+
+public class RestTest {
+
+    @Test
+    public void SerialisedTest(){
+        Order requestorder = new Order();
+
+        requestorder.setStatus("OPEN");
+        requestorder.setCourierId(123);
+        requestorder.setCustomerName("JoJo");
+        requestorder.setCustomerPhone("12345678");
+        requestorder.setComment("Faster!");
+
+        Gson gson = new Gson();
+        String StringRequestOrder = gson.toJson(requestorder);
+
+        given().contentType("application/json")
+                .body(StringRequestOrder)
+        .when()
+                .post("http://51.250.6.164:8080/test-orders")
+        .then()
+                .statusCode(200)
+                .body("status",equalTo(requestorder.getStatus()));
+    }
+}
